8553dc10abdd36f70f50d88891acaa39a8b4be58
jccjd <1340944538@qq.com>
2019-03-09 17:47:23 +0800

    字符串常见操作

--- 字符串/test.py
+++ 字符串/test.py
@@ -12,3 +12,18 @@ c = 'zhao'
 d = a+b
 print(d)
 #字符串切片
+name = 'adcdefABCDEF'
+# print(name[2:4]) -->cd
+# name[2:-1] cd--E 倒数第二个
+# name[2:]
+# name[2:-1:2] 跳一个取
+#倒序 name[-1:0:-1] name[::-1]
+#字符串常见操作
+myStr = 'hello world itcast and itcastxxcpp'
+print(myStr.find('world'))
+print(myStr.count('world'))
+#替换 不可对原始值操作
+print(myStr.replace('world','wolld'))
+print(myStr)
+#切割
+print(myStr.split(" "))
