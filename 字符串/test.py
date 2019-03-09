12a63b86a92cb5cc00e69c731951dbbd93da3384
jccjd <1340944538@qq.com>
2019-03-09 12:28:26 +0800

    pythonLearning

---
+++ 字符串/test.py
@@ -0,0 +1,14 @@
+num = 100
+num2 ="100"
+name = 'laowang'
+int (num2)
+str(num)
+print (len(name))
+# userNme = input('输入用户名：')
+# 字符串加法
+a = 'lao'
+b = 'wang'
+c = 'zhao'
+d = a+b
+print(d)
+#字符串切片
