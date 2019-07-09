84d07101256e49b8ab51fed012ce92ef66ad5127
jccjd <1340944538@qq.com>
2019-07-09 08:49:26 +0800

    reviewpython

--- reviewPython/demo.py
+++ reviewPython/demo.py
@@ -1,32 +1,15 @@
 a = "a"
-print(id(a))#140721507197776
+# print(id(a))#140721507197776
 a = "b"
-print(id(a))#140721507197808
+# print(id(a))#140721507197808
 
 a = []
 a.append(1)
-print(id(a))#2687715074696
+# print(id(a))#2687715074696
 a.append(2)
-print(a)
-#单例模式
-class Singleton(type):
-    def __init__(cls,name,base,dict):
-        super(Singleton,cls).__init__(name,base, dict)
-        cls._instance = None
-    def __call__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(Singleton,cls).__call__(*args, **kwargs)
-        return cls._instance
-class Singleton(object):
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls,'_instance'):
-            cls._instance = super(Singleton,cls).__new__(cls,*args,**kwargs)
-        return cls._instance
+# print(id(a))#2687715074696
 
 
-class MyClass3(metaclass = Singleton):
-    def foo(self):
-        pass
 
 
 def foo(x):
@@ -41,13 +24,6 @@ class A(object):
     def static_foo(x):
         print("executing static_foo(%s)"%x)
 
-if __name__ == '__main__':
-
-    one = MyClass3()
-    print(id(one))
-    two = MyClass3()
-    print(id(two))
-    foo(1)
-    a = A()
-    a.foo(1)
-    A.class_foo(1)
+a = range(1,10)
+print(type(a))
+print(a)
