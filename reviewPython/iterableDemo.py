6577d619ecf0bdad874dbd061fc1d575e6a999f5
jccjd <1340944538@qq.com>
2019-07-16 21:21:28 +0800

    reviewpython

--- reviewPython/iterableDemo.py
+++ reviewPython/iterableDemo.py
@@ -1,2 +1,3 @@
 print(dir([1,2]))
-print(type([1,2]))
+print(dir("qqqq"))
+print(dir([1,2].__iter__()))
--- reviewPython/quick.py
+++ reviewPython/quick.py
@@ -18,6 +18,26 @@ def quick_sort(lists,left,right):
     quick_sort(lists,left + 1 ,high)
     return lists
 
-list = [9,2,13,4,563,2]
+list = [2,5,7,6,3,1]
 l = quick_sort(list,0,5)
 print(l)
+def demo_quick_sorting(lists, left, right):
+    if left > right:
+        return lists
+    key = lists[left]
+    low = left
+    height = right
+
+    while left < right:
+        if left < right and lists[right] > key:
+            right -= 1
+        lists[left] = lists[right]
+        if left < right and lists[left] <= key:
+            left += 1
+        lists[right] = lists[left]
+    lists[right] = key
+
+    demo_quick_sorting(lists,low,left - 1)
+    demo_quick_sorting(lists,left + 1,height)
+
+
