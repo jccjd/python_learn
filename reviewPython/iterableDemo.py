b69e2fac903b1278585443e2678a1cfc245a3c15
jccjd <1340944538@qq.com>
2019-07-17 14:45:42 +0800

    reviewpython

--- reviewPython/iterableDemo.py
+++ reviewPython/iterableDemo.py
@@ -1,8 +1,7 @@
-class MyNumbers:
+class myrange:
     def __iter__(self):
         self.a = 1
         return self
-
     def __next__(self):
         if self.a <= 20:
             x = self.a
@@ -11,9 +10,13 @@ class MyNumbers:
         else:
             raise StopIteration
 
+iters = myrange()
+iters = iter(iters)
 
-myclass = MyNumbers()
-myiter = iter(myclass)
-
-for x in myiter:
-    print(x)
+list = [1,2,3,4]
+iters = iter(list)
+while True:
+    try:
+        print(next(iters))
+    except StopIteration:
+        break
