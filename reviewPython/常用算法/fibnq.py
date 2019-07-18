a1f9a4b46aac3c1e5718933e59fc5e0ad36e5d17
jccjd <1340944538@qq.com>
2019-07-18 14:08:52 +0800

    reviewpython

--- reviewPython/常用算法/fibnq.py
+++ reviewPython/常用算法/fibnq.py
@@ -4,4 +4,12 @@ def fib(i):
     if i < 2:
         return 1
     return fib(i-1) + fib(i-2)
-print(fib(6))
+def fib(n):
+    fib = []
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b , a + b
+        fib.append(b)
+    return fib
+
+print(fib(20))
