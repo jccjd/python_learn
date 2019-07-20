b88b9d9f6c80a63ca68c66ae1f66ac19954a311d
jccjd <1340944538@qq.com>
2019-07-20 14:24:46 +0800

    reviewpython

--- reviewPython/demometaclss.py
+++ reviewPython/demometaclss.py
@@ -1,3 +1,4 @@
+
 def upper_attr(future_class_name,future_class_parents,future_class_attr):
     '''返回一个类，将属性都大写 version1.0'''
     attrs = ((name,value) for name,value in future_class_attr.items() if not name.startswith('__'))
@@ -14,41 +15,51 @@ class UpperAttrMetaClass(type):
     #     uppercase_attrs = dict((name.upper(),value) for name, value in attrs)
     #     return type.__new__(cls,name,bases,uppercase_attrs)
 
-    def __new__(cls,name,bases,dct):
-        '''返回一个类，将属性都大写 version3.0'''
-        attrs = ((name,value) for name,value in dct.items() if not name.startswith('__'))
-        uppercase_attrs = dict((name.upper(),value) for name, value in attrs)
+    def __new__(mcs, name, bases, dct):
+        attrs = ((name, value) for name, value in dct.items() if not name.startswith('__'))
+        uppercase_attrs = dict((name.upper(), value) for name, value in attrs)
 
-        return super(UpperAttrMetaClass,cls).__new__(cls,name,bases,uppercase_attrs)
-class Foo(metaclass=UpperAttrMetaClass):
-    bar = 'bip'
-f = Foo()
-print(f.BAR)
+        return super(UpperAttrMetaClass, mcs).__new__(mcs, name, bases, uppercase_attrs)
+
+
 
 class MetaClassModel(type):
     def __new__(mcs, name, base, dct):
-        if name == 'Model':
-            return super(MetaClassModel, mcs).__new__(mcs, name, base, dct)
-        mapping = dict()
-        for k, v in dct.items():
-            mapping[k] = v
-        for k in mapping.keys():
-            dct.pop(k)
-        dct['mapping'] = mapping
 
+        if name == 'Model':
+           return super(MetaClassModel, mcs).__new__(mcs, name, base, dct)
+
+        attrs = ((name, value) for name, value in dct.items() if not name.startswith('__'))
+        mapping = dict((name, value) for name, value in attrs)
+
+        for k in mapping:
+            dct.pop(k)
+        dct['__mapping__'] = mapping
         return super(MetaClassModel, mcs).__new__(mcs, name, base, dct)
 
-class Model(dict, metaclass=MetaClassModel):
-    def __init__(self,*args, **kwargs):
-        super(Model,self).__init__(self,*args, **kwargs)
+class Model(metaclass=MetaClassModel):
 
     def save(self):
-        for k, v, in self.mapping.items():
-            print(k,v)
-class Student(Model):
-    id = 'df'
-    cd = 'df'
-stu = Student(id='d',cd = 'fd')
-stu.save()
+        for k, v in self.__mapping__.items():
+            print(k, v)
 
 
+class User(Model):
+    id = 1
+    name = 'll'
+    sex = 'll'
+
+class Singleton(type):
+    def __init__(cls, name, base, dct):
+        super(Singleton, cls).__init__(name, base, dct)
+        cls._instance = None
+    def __call__(self, *args, **kwargs):
+        if self._instance == None:
+            self._instance = super(Singleton, self).__call__(*args, **kwargs)
+        return self._instance
+
+class boo(metaclass=Singleton):
+    pass
+f = boo()
+b = boo()
+print(f is b)
