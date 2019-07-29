d9af6015ea8e8521aae767627ffef8bf6b0c9c36
jccjd <1340944538@qq.com>
2019-07-29 21:44:19 +0800

    reviewpython

--- 数据结构和算法/sort1.py
+++ 数据结构和算法/sort1.py
@@ -1,37 +1,37 @@
 list = [2,1,3,4,5]
 length = len(list)
-
 def bubble(list):
     for i in range(length):
-      for j in range(length - 1):
-          if list[j] > list[j + 1]:
-              list[j], list[j + 1] = list[j + 1], list[j]
-    return list
-def InsertSort(list):
-    for i in range(1,length):
-        for j in range(i -1, -1, -1):
+        for j in range(length - 1 ):
             if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + j], list[j]
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+def Instertsort(list):
+    for i in range(length):
+        for j in range(i - 1, -1, -1):
+            if list[j] >list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
     return list
 
-def selectSort(list):
+def selectsort(list):
     for i in range(length):
         min = list[i]
-        for j in range(i + 1, length):
+        for j in range(i+1,length):
             if list[j] < min:
-               list[j], min = min, list[j]
+                list[j], min = min,list[j]
         list[i] = min
     return list
-def quickSort(list):
-    if len(list) <= 1:
+def quicksort(list):
+    if len(list) <=1:
         return list
     key = list[len(list) // 2]
     left = [i for i in list if i < key]
     middle = [i for i in list if i == key]
-    right = [i for i in list if i > key]
-    return quickSort(left) + middle + quickSort(right)
+    rigth = [i for i in list if i > key]
 
-print(quickSort(list))
-print(selectSort(list))
-print(InsertSort(list))
-print(bubble([2,1,3,4,5]))
+    return quicksort(left) + middle + quicksort(rigth)
+print(quicksort(list))
+print(bubble(list))
+print(Instertsort(list))
+print(selectsort(list))
+
