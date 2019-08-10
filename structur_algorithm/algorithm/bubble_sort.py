faf0650157f39aaa16d2688d677a9fd094a864a7
jccd <1340944538@qq.com>
2019-08-10 16:22:21 +0800

    network base

--- structur_algorithm/algorithm/bubble_sort.py
+++ structur_algorithm/algorithm/bubble_sort.py
@@ -1,10 +1,3 @@
-# def Bubblesort(numbs):
-#     for i in range(len(numbs)):
-#         for j in range(len(numbs) - 1):
-#             if numbs[j] > numbs[j + 1]:
-#                 numbs[j], numbs[j + 1] = numbs[j + 1], numbs[j]
-#
-#     return numbs
 
 def Bubblesort(nums):
     for i in range(len(nums)):
@@ -12,6 +5,16 @@ def Bubblesort(nums):
             if nums[j] > nums[j + 1] :
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
+    return nums
+def Bubble(nums):
+    for i in range(len(nums)):
+        flag = True
+        for j in range(len(nums) - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                flag = False
+            if flag == True:
+                break
     return nums
 
 nums = [5, 4, 3, 2, 1]
--- structur_algorithm/algorithm/insertsort.py
+++ structur_algorithm/algorithm/insertsort.py
@@ -5,6 +5,12 @@ def Insertsort(numbs):
                 numbs[j], numbs[j + 1] = numbs[j + 1], numbs[j]
 
     return numbs
+def Insert(numbs):
+    for i in range(len(nums)):
+        for j in range(i - 1, -1, -1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
+    return nums
 nums = [5, 4, 3, 2, 1]
 print(Insertsort(nums))
